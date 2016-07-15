65a9069a9ac44e33ab14e8270797367ed415d2c3
yahya <kittaneh@gmail.com>
2016-07-15 21:46:53 +0300

    cleaning

--- src/test/groovy/rnd/grails3/httpinvoker/producer/BookServiceSpec.groovy
+++ src/test/groovy/rnd/grails3/httpinvoker/producer/BookServiceSpec.groovy
@@ -4,9 +4,7 @@ import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
+
 @TestFor(BookService)
 @Mock([Book])
 class BookServiceSpec extends Specification {
